969ef78f79825b8475d9baad090dc2b791a8b98b
조현아 <minjokwindows5@gmail.com>
2024-05-14 12:03:39 +0900

    Initial commit

---
+++ src/test/java/org/sparta/spring_basic_project/SpringBasicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.spring_basic_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
